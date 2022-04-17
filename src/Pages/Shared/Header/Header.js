265ec23de791119fca81b25f749d33cba4b5e175
Khaled Karim <kskarim177@gmail.com>
2022-04-17 15:45:14 +0600

    getting the routes ready

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    // const [user] = useAuthState (auth)
     return (
-        <div>
-            <h1>I am the head of the site</h1>
-        </div>
+        <>
+            <Navbar collapseOnSelect sticky='top' expand="lg" bg="secondary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Your Life Coach</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to='/home'>Home</Nav.Link>
+                            <Nav.Link href="home#services">Services</Nav.Link>
+                            <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
+                            <Nav.Link as={Link} to='/about'>About</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Login</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Log Out
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
