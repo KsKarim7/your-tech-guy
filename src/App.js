27cc07d21aa3d4151c74a6e88526033f0798ae38
Khaled Karim <kskarim177@gmail.com>
2022-04-17 14:58:41 +0600

    getting the routes ready

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
-import Header from './Pages/Shared/Header/Header'
-import Footer from './Pages/Shared/Footer/Footer'
-import { Routes } from 'react-router-dom';
+import Header from './Pages/Shared/Header/Header';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Registration/Login/Login';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
+import Checkout from './Pages/Checkout/Checkout';
+import Blogs from './Pages/Blogs/Blogs';
+import About from './Pages/About/About';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,9 +16,16 @@ function App() {
       <Header></Header>
 
       <Routes>
-
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/serviceDetail' element={<ServiceDetail></ServiceDetail>}></Route>
+        <Route path='/checkout' element={<Checkout></Checkout>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        {/* <Route path='/' element={ }></Route> */}
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
-
       <Footer></Footer>
     </div>
   );
